7ca24c756f5d14f31e1c8bb54beec35db4fb6a9b
Levi <phamnguyentanphucgl@gmail.com>
2023-06-27 20:34:32 +0700

    build bottom navigate

---
+++ lib/feature/float_bottom_navigation/cubit/bottom_navigation_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:study_abroad_consultant/feature/home/router/home_wrapper_router.dart';
+import 'package:study_abroad_consultant/utils/colors.dart';
+import 'package:study_abroad_consultant/widgets/not_found_page.dart';
+
+part 'bottom_navigation_state.dart';
+
+class BottomNavigationBloc extends Cubit<BottomNavigationState> {
+  BottomNavigationBloc() : super(const BottomNavigationState());
+
+  void onItemTapped(int index) =>
+      _emitIfOpen(state.copyWith(pageIndex: PageIndex.values[index]));
+  void _emitIfOpen(BottomNavigationState newState) {
+    if (!isClosed) {
+      emit(newState);
+    }
+  }
+}
---
+++ lib/feature/float_bottom_navigation/cubit/bottom_navigation_state.dart
@@ -0,0 +1,75 @@
+part of 'bottom_navigation_bloc.dart';
+
+class BottomNavigationState extends Equatable {
+  final PageIndex pageIndex;
+
+  const BottomNavigationState({this.pageIndex = PageIndex.home});
+
+  @override
+  List<Object> get props => [
+        pageIndex,
+      ];
+
+  BottomNavigationState copyWith({
+    PageIndex? pageIndex,
+  }) {
+    return BottomNavigationState(
+      pageIndex: pageIndex ?? this.pageIndex,
+    );
+  }
+}
+
+enum PageIndex {
+  home,
+  myUser,
+}
+
+extension PageIndexExt on PageIndex {
+  String lableOf() {
+    switch (this) {
+      case PageIndex.home:
+        return "Home";
+
+      case PageIndex.myUser:
+        return "My User";
+      default:
+        return '';
+    }
+  }
+
+  Widget iconOf({bool isActived = false}) {
+    switch (this) {
+      case PageIndex.home:
+        return Icon(
+          Icons.home,
+          size: 30,
+          color: isActived ? XColors.primary : null,
+        );
+
+      case PageIndex.myUser:
+        return Icon(
+          Icons.person,
+          size: 30,
+          color: isActived ? XColors.primary : null,
+        );
+      default:
+        return const Icon(Icons.emoji_emotions_outlined);
+    }
+  }
+
+  Widget get page {
+    switch (this) {
+      case PageIndex.home:
+        return const HomeWrapperPage();
+      // case PageIndex.calendar:
+      //   return const ScheduleWrapperPage();
+      // case PageIndex.studentCard:
+      //   return const CardStudentWrapperPage();
+      case PageIndex.myUser:
+      //   return const MyUserWrapperPage();
+
+      default:
+        return const NotFoundPage();
+    }
+  }
+}
